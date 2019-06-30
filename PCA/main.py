635522a29f0ff42ad69a093b2bd9445610fcfa56
Can Huang <hc525747310@163.com>
2019-06-30 19:02:48 +0800

    PCA案例更新

--- PCA/main.py
+++ PCA/main.py
@@ -43,11 +43,92 @@ plt.show()
 #数据预处理
 from sklearn.preprocessing import StandardScaler
 X_std = StandardScaler().fit_transform(X)
-print ('X_std:',X_std)
+#print ('X_std:',X_std)
 
 #计算协方差矩阵
 mean_vec = np.mean(X_std, axis=0)
 cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
 print('Covariance matrix \n%s' %cov_mat)
 
+#用numpy的函数计算协方差矩阵
 print('NumPy covariance matrix: \n%s' %np.cov(X_std.T))
+
+cov_mat = np.cov(X_std.T)
+
+#eig_vals：特征值；eig_vecs：特征向量；一个特征值对应一个特征向量
+eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+
+print('Eigenvectors \n%s' %eig_vecs)
+print('\nEigenvalues \n%s' %eig_vals)
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
+print (eig_pairs)
+print ('----------')
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eig_pairs.sort(key=lambda x: x[0], reverse=True)
+
+# Visually confirm that the list is correctly sorted by decreasing eigenvalues
+print('Eigenvalues in descending order:')
+for i in eig_pairs:
+    print(i[0])
+
+tot = sum(eig_vals)
+var_exp = [(i / tot)*100 for i in sorted(eig_vals, reverse=True)]
+print (var_exp)
+#np.cumsum：每一个值是把前面的值加在一起
+cum_var_exp = np.cumsum(var_exp)
+print(cum_var_exp)
+
+
+'''a = np.array([1,2,3,4])
+print (a)
+print ('-----------')
+print (np.cumsum(a))'''
+
+plt.figure(figsize=(6, 4))
+
+plt.bar(range(4), var_exp, alpha=0.5, align='center',
+            label='individual explained variance')
+plt.step(range(4), cum_var_exp, where='mid',
+             label='cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+matrix_w = np.hstack((eig_pairs[0][1].reshape(4,1),
+                      eig_pairs[1][1].reshape(4,1)))
+
+print('Matrix W:\n', matrix_w)
+
+Y = X_std.dot(matrix_w)
+print(Y)
+
+plt.figure(figsize=(6, 4))
+for lab, col in zip(('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'),
+                        ('blue', 'red', 'green')):
+     plt.scatter(X[y==lab, 0],
+                X[y==lab, 1],
+                label=lab,
+                c=col)
+plt.xlabel('sepal_len')
+plt.ylabel('sepal_wid')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(6, 4))
+for lab, col in zip(('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'),
+                        ('blue', 'red', 'green')):
+     plt.scatter(Y[y==lab, 0],
+                Y[y==lab, 1],
+                label=lab,
+                c=col)
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.legend(loc='lower center')
+plt.tight_layout()
+plt.show()
+
